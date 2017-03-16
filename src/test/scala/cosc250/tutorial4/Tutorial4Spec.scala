d2e3a602ee0552ed54143355fddf20d9e1ee9161
William Billingsley <wbilling@une.edu.au>
2017-03-17 10:11:07 +1100

    Added empty test script

---
+++ src/test/scala/cosc250/tutorial4/Tutorial4Spec.scala
@@ -0,0 +1,17 @@
+package cosc250.tutorial4
+
+import org.scalatest._
+
+import cosc250.tutorial4.SI._
+
+
+/**
+  *
+  */
+class Tutorial4Spec extends FlatSpec with Matchers {
+
+  "you" should "write some tests for your code and delete this"  in {
+    false should be (true)
+  }
+
+}
